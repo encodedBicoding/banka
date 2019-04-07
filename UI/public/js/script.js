095b7aeb8e79ea864684d1aaa714b426d1db1990
encodedBicoding <dominicisioma000@gmail.com>
2019-04-07 10:30:00 +0100

    refactor(UI):update on responsiveness

--- UI/public/js/script.js
+++ UI/public/js/script.js
@@ -1,17 +1,20 @@
 
-const toggle = (e)=>{
-    e.style.display === 'block'? e.style.display = 'none' : e.style.display = 'block';
-    return e.style.display;
+const toggle = (e) => {
+  if (e.style.display === 'block') {
+    e.style.display = 'none';
+  } else {
+    e.style.display = 'block';
+  }
+  return e.style.display;
 };
-function showDashNav(){
-    let button = document.getElementById('toggle_nav');
-    let nav = document.getElementById('user_nav');
-    button.addEventListener('click', (event)=>{
-        let span = document.getElementById('xx');
-        event.preventDefault();
-        toggle(nav) === 'block' ?
-        span.className = 'fa fa-times':span.className = 'fa fa-bars';
-    })
-
+function showDashNav() {
+  const button = document.getElementById('toggle_nav');
+  const nav = document.getElementById('user_nav');
+  button.addEventListener('click', (event) => {
+    const span = document.getElementById('xx');
+    event.preventDefault();
+    toggle(nav) === 'block'
+      ? span.className = 'fa fa-times' : span.className = 'fa fa-bars';
+  });
 }
 showDashNav();
