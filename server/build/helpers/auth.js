8451dbfc301caa82f68a4fb69103947160784f71
encodedBicoding <dominicisioma000@gmail.com>
2019-04-12 16:49:06 +0100

    chore(babel):transpiled code with babel [Finsihes #165315136]

---
+++ server/build/helpers/auth.js
@@ -0,0 +1,51 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports["default"] = void 0;
+
+var _jsonwebtoken = _interopRequireDefault(require("jsonwebtoken"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
+
+function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
+
+var secretKey = 'catsanddogs';
+
+var Auth =
+/*#__PURE__*/
+function () {
+  function Auth() {
+    _classCallCheck(this, Auth);
+  }
+
+  _createClass(Auth, null, [{
+    key: "generateToken",
+    value: function generateToken(payload) {
+      return _jsonwebtoken["default"].sign(payload, secretKey, {
+        expiresIn: '1week'
+      });
+    }
+  }, {
+    key: "verifyToken",
+    value: function verifyToken(token) {
+      try {
+        return _jsonwebtoken["default"].verify(token, secretKey);
+      } catch (e) {
+        return {
+          error: "".concat(e)
+        };
+      }
+    }
+  }]);
+
+  return Auth;
+}();
+
+var _default = Auth;
+exports["default"] = _default;
