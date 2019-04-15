565a1bfee26922910dd18e5f8b64b58a5d8da3f1
encodedBicoding <dominicisioma000@gmail.com>
2019-04-15 16:38:27 +0100

    chore(add-env-file):adds env filr for all environment variable use by the app

--- server/app.js
+++ server/app.js
@@ -1,16 +1,16 @@
 import express from 'express';
-import dotenv from 'dotenv';
+import dotEnv from 'dotenv';
 import debug from 'debug';
 import config from './config/config';
 
-dotenv.config();
+dotEnv.config();
 const logger = debug('banka');
 
 const app = express();
 
 config(app);
 
-const PORT = process.env.PORT || 2046;
+const PORT = process.env.PORT || 2029;
 app.listen(PORT, logger(`app running on PORT ${PORT}`));
 
 export default app;
