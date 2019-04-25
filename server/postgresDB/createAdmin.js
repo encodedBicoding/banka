cfad9ca1ef36fb120352baaf3474c2963ac68e08
encodedBicoding <dominicisioma000@gmail.com>
2019-04-25 16:57:19 +0100

    bug(heroku):adds admin creatials to .env files for safe keeping

--- server/postgresDB/createAdmin.js
+++ server/postgresDB/createAdmin.js
@@ -1,10 +1,17 @@
+import { config } from 'dotenv';
 import pool from './DB/dbConnection';
 
+config();
+
+const adminFirstName = process.env.ADMIN_FIRSTNAME;
+const adminLastName = process.env.ADMIN_LASTNAME;
+const adminEmail = process.env.ADMIN_EMAIL;
+const adminPassword = process.env.ADMIN_PASSWORD;
 const query2 = 'INSERT INTO staffs(firstname, lastname, email, password, type) VALUES($1, $2, $3, $4, $5)';
-const value = ['dominic',
-  'isioma',
-  'dominicisioma000@gmail.com',
-  '$2b$10$0WIvCrJLph9ZDK2EUjrGYOdHMln7EdLwgHNntY13XMLlfxcAidjoe',
+const value = [adminFirstName,
+  adminLastName,
+  adminEmail,
+  adminPassword,
   'admin'];
 
 pool.query(query2, value, (err, res) => {
